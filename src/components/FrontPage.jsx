5f6cc034f5c24f669589c40529bbc38ea2fdb678
Derek Cooper <derekcooper20@gmail.com>
2023-06-26 15:47:11 -0600

    adds fp

---
+++ src/components/FrontPage.jsx
@@ -0,0 +1,11 @@
+import './frontpage.css'
+
+import React from 'react'
+
+export const FrontPage = () => {
+  return (
+    <div className='frontpage-container'>
+      <h1>Front Page</h1>
+    </div>
+  )
+}
