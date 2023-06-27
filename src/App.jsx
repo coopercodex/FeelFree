2f3f63030ca741bfb39ee6f4bd47699718c3b615
Derek Cooper <derekcooper20@gmail.com>
2023-06-27 15:25:37 -0600

    Adds data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { FrontPage } from './components/FrontPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
 
   return (
     <>
       <Navbar />
       <Routes>
-        {/* <Route path='/' element={<FrontPage />} /> */}
+        <Route path='/' element={<FrontPage />} />
       </Routes>
     </>
   )
--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,11 +1,32 @@
 import './frontpage.css'
+import requests from '../../requests'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const FrontPage = () => {
+  const [movies, setMovies] = useState([])
+  const baseUrl = "https://image.tmdb.org/t/p/original"
+
+
+  const getData = () => {
+    fetch(requests.horrorMovies)
+      .then(res => res.json())
+      .then(data => setMovies(data.results[Math.floor(Math.random() * data.results.length - 1)]))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className='frontpage-container'>
-      <h1>Front Page</h1>
+      <h1>Just In</h1>
+        <h2>{movies.title || movies.name || movies.original_name}</h2>
+      <div className='justin-container'>
+        <img className='justin-picture' src={`${baseUrl}${movies?.poster_path || movies?.backdrop_path }`} alt={movies?.name} />
+        <p>{movies.overview}</p>
+      </div>
     </div>
   )
 }
